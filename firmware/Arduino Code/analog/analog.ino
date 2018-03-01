6d36fb566a463e71b91ee5c89359a909ec564f86
AidenRay <aidenunsw@outlook.com>
2018-03-01 18:22:44 +1100

    writing c++ classes for scheduling using inbuilt timers

---
+++ firmware/Arduino Code/analog/analog.ino
@@ -0,0 +1,23 @@
+#define PIN_ANALOG_TEMP 9
+#define PIN_ANALOG_LIGHT 8
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_ANALOG_TEMP, INPUT);
+  pinMode(PIN_ANALOG_LIGHT, INPUT);
+}
+
+//initialise variables
+int temperatureADC = 0;
+int light = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  temperatureADC = analogRead(PIN_ANALOG_TEMP);
+  light = analogRead(PIN_ANALOG_LIGHT);
+  Serial.print(temperatureADC);
+  Serial.println(light);
+  delay(100);
+
+}
