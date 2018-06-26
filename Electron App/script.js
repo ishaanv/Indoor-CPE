c9472487f99e62642248f23e1bab033ae0a6a524
Ben Doherty <Ben_Doherty@bvn.com.au>
2018-06-26 19:49:07 +1000

    Add js linter

--- Electron App/script.js
+++ Electron App/script.js
@@ -1,27 +1,36 @@
-const {remote} = require('electron')
+const {remote} = require('electron');
 
-//https://discuss.atom.io/t/how-to-catch-the-event-of-clicking-the-app-windows-close-button-in-electron-app/21425/7
+// https://discuss.atom.io/t/how-to-catch-the-event-of-clicking-the-app-windows-close-button-in-electron-app/21425/7
 
 
 document.getElementById('close').addEventListener('click', closeWindow);
 document.getElementById('minimise').addEventListener('click', minimiseWindow);
 document.getElementById('maximise').addEventListener('click', maximiseWindow);
 
+/**
+ * Closes the window
+ */
 function closeWindow() {
-    console.log("I'm tryna close")
+    console.log('I\'m tryna close');
     window.close();
 }
 
+/**
+ * Minimises the window
+ */
 function minimiseWindow() {
-    console.log("I'm tryna minimise")
-    var window = remote.getCurrentWindow()
-    window.minimize()
+    console.log('I\'m tryna minimise');
+    let window = remote.getCurrentWindow();
+    window.minimize();
 }
 
+/**
+ * Maximises the window
+ */
 function maximiseWindow() {
-    console.log("I'm tryna maximise")
-    var window = remote.getCurrentWindow()
-    if(window.isMaximized()) {
+    console.log('I\'m tryna maximise');
+    let window = remote.getCurrentWindow();
+    if (window.isMaximized()) {
         window.unmaximize();
     } else {
         window.maximize();
