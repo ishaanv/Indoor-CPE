8601edc7161a7bf0f551f0aeeb7addcb504e818f
AidenRay <aidenunsw@outlook.com>
2018-06-27 16:37:53 +1000

    10 mins of data collected in shim.json

--- firmware/CPE-circuitPython/serial_test.py
+++ firmware/CPE-circuitPython/serial_test.py
@@ -1,7 +1,9 @@
 import serial
 import time
 
-ser = serial.Serial('COM3', 
+import re
+
+ser = serial.Serial('COM12', 
                     baudrate = 115200,
                     parity=serial.PARITY_NONE,
                     stopbits=serial.STOPBITS_ONE,
@@ -16,7 +18,19 @@ while True:
     # send = input("input:")
     # x = ser.readline()
     # ser.write((str(i)+'\r\n').encode())
-    print(ser.readline())
+    line = str(ser.readline())
+    if 'sound' in line:
+        sound = re.findall("\d+\.\d+",line)
+        print(sound[0])
+
+    elif 't:' in line:
+        temp = re.findall("\d+\.\d+",line)
+        print(temp[0])
+    elif 'l:' in line:
+        light = re.findall("\d+\.\d+",line)
+        print(light[0])
+
+
 
     i += 1
     # time.sleep(1)
