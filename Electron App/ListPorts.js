f37e6b5d81e32413cac9b13463250229879f24ba
AidenRay <aidenunsw@outlook.com>
2018-06-28 17:41:05 +1000

    Print serial port values to console

---
+++ Electron App/ListPorts.js
@@ -0,0 +1,14 @@
+var serialport = require("serialport")
+var serialport = serialport
+
+//list serial ports:
+serialport.list(function (err, ports) {
+    ports.forEach(function(port) {
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+    })
+
+
+
+})
