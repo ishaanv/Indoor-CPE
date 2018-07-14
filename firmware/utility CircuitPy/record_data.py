aad2646d4025d231458a2bb3cfb69bca0568b178
Ben Doherty <ben@notionparallax.co.uk>
2018-07-14 16:36:27 +1000

    refactor regex mainly to keep the linter happy, but a bit for readability

--- firmware/utility CircuitPy/record_data.py
+++ firmware/utility CircuitPy/record_data.py
@@ -21,25 +21,27 @@ data = {"temperature": [], "soundVolume": []}
 now = time.time()
 mins = 10
 future = now + mins * 60
+float_regex = r"\d+\.\d+"
+
 while time.time() < future:
 
     line = str(ser.readline())
     if 'sound' in line:
-        sound = re.findall("\d+\.\d+", line)
+        sound = re.findall(float_regex, line)
         data["soundVolume"].append({
             "time": dt.datetime.now().isoformat(),
             "value": sound[0]
         })
 
     elif 't:' in line:
-        temp = re.findall("\d+\.\d+", line)
+        temp = re.findall(float_regex, line)
         data["temperature"].append({
             "time": dt.datetime.now().isoformat(),
             "value": temp[0]
         })
         print(temp[0])
     elif 'l:' in line:
-        light = re.findall("\d+\.\d+", line)
+        light = re.findall(float_regex, line)
         # print(light[0])
     if (future - time.time()) % 60 == 0:
         print(time.time())
