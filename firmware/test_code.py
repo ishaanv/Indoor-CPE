877ab251721d5aec5b1570a86586238032096158
AidenRay <AidenRay@github.com>
2018-08-01 15:24:28 +1000

    Fix import error

--- firmware/test_code.py
+++ firmware/test_code.py
@@ -2,9 +2,9 @@ from code_dev import mean
 
 
 def test_mean():
-    assert code.mean([1, 3]) == 2
-    assert code.mean([0, 0]) == 0
-    assert code.mean([0]) == 0
+    assert mean([1, 3]) == 2
+    assert mean([0, 0]) == 0
+    assert mean([0]) == 0
     # assert mean([]) = NaN
     # assert mean([12345678901234567890, 12345678901234567890])
 
