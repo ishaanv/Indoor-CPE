6a259d5cb573a8f13bfc6479ff3ad9ba394878e5
AidenRay <AidenRay@github.com>
2018-08-01 15:17:19 +1000

    mild edit to test code

--- firmware/test_code.py
+++ firmware/test_code.py
@@ -1,10 +1,12 @@
-from code import cpmean
+# from code import mean
+import code
 
 def test_mean():
-    assert mean([1, 3]) == 2
-    assert mean([0, 0]) == 0
-    assert mean([0]) == 0
+    assert code.mean([1, 3]) == 2
+    assert code.mean([0, 0]) == 0
+    assert code.mean([0]) == 0
     # assert mean([]) = NaN
     # assert mean([12345678901234567890, 12345678901234567890])
-    
+
+
 test_mean()
